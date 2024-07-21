8621ca0d42cc9ce0c3c5992a7ad3b7ec35273743
joelmuraguri <joelkmuraguri21@gmail.com>
2024-07-21 20:19:17 +0300

    data di

---
+++ core/data/src/main/java/com/joel/data/di/DataModule.kt
@@ -0,0 +1,28 @@
+package com.joel.data.di
+
+import android.content.Context
+import com.joel.data.repository.connectivity.ConnectivityObserver
+import com.joel.data.repository.connectivity.DefaultConnectivityObserver
+import com.joel.data.repository.forecast.ForecastRepository
+import com.joel.data.repository.forecast.ForecastRepositoryImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal interface DataModule {
+
+    @Provides
+    fun providesContext(@ApplicationContext context: Context): Context = context
+
+    @Binds
+    fun bindsConnectivityRepository(connectivityRepo : DefaultConnectivityObserver): ConnectivityObserver
+
+    @Binds
+    fun bindsForecastRepository(forecastRepositoryImpl: ForecastRepositoryImpl): ForecastRepository
+
+}
---
+++ core/data/src/main/java/com/joel/data/di/LocationModule.kt
@@ -0,0 +1,45 @@
+package com.joel.data.di
+
+import android.content.Context
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Granularity
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.joel.data.repository.location.DefaultLocationClient
+import com.joel.data.repository.location.LocationClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+private const val TIME_INTERVAL_IN_MILLIS = 1000L
+private const val MINIMAL_DISTANCE = 10000f
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocationModule {
+
+    @Provides
+    fun provideFusedLocationProvider(context: Context) =
+        LocationServices.getFusedLocationProviderClient(context)
+
+    @Provides
+    fun providesContext(@ApplicationContext context: Context): Context = context
+
+    @Provides
+    fun providesLocationRequest() =
+        LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, TIME_INTERVAL_IN_MILLIS).apply {
+            setMinUpdateDistanceMeters(MINIMAL_DISTANCE)
+            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+    @Provides
+    fun providesLocationDataSource(
+        locationRequest: LocationRequest,
+        fusedLocationProviderClient: FusedLocationProviderClient
+    ): LocationClient = DefaultLocationClient(fusedLocationProviderClient, locationRequest)
+
+}
