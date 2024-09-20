43ba99655a0481853761dd28b900ba427e7294f7
joelmuraguri <joelkmuraguri21@gmail.com>
2024-09-20 14:32:26 +0300

    reverse geocoding

---
+++ core/network/src/main/java/com/joel/network/service/ReverseGeoService.kt
@@ -0,0 +1,18 @@
+package com.joel.network.service
+
+import com.joel.network.models.ReverseGeoCodingResponse
+import com.skydoves.sandwich.ApiResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface ReverseGeoService {
+
+    @GET("reverse")
+    suspend fun getLocationAddress(
+        @Query("lat") latitude : Double,
+        @Query("lon") longitude : Double,
+        @Query("api_key") apiKey : String = "66ed3e1a48139365675555wom5c1f33",
+    ) : ApiResponse<ReverseGeoCodingResponse>
+
+}
